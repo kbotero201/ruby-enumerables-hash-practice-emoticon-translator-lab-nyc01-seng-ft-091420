167b7fbae76ac257e55f354625f2967e1605bfa1
Kevin Botero <kbotero201@gmail.com>
2020-09-11 21:12:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,15 +4,9 @@ require "pry"
 
 def load_library(path)
   # code goes here
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
-  end
-  emoji_hash
+  def load_library(file_path)
+  file = File.read(file_path)
+  emoticons = YAML.load(file)
 end
 
 
