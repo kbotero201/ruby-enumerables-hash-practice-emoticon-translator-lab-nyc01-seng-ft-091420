f92001d5c1a305268bf433fdbfecaaa87b23df5d
Kevin Botero <kbotero201@gmail.com>
2020-09-11 21:08:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,12 @@ def get_japanese_emoticon(path, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  emoji = load_library(path)
+  if emoji["get_meaning"].has_key?(emoticon)
+    emoji["get_meaning"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
